7a1fe72be2637a1449668fa2ce132102038a2817
perottiriccardo <riccardo.perotti@outlook.it>
2024-01-27 19:41:06 +0100

    Copyright

--- launcher.py
+++ launcher.py
@@ -126,36 +126,19 @@ def main():
 
 
 
-    footer="""<style>
-    a:link , a:visited{
-    color: blue;
-    background-color: transparent;
-    text-decoration: underline;
-    }
-
-    a:hover,  a:active {
-    color: red;
-    background-color: transparent;
-    text-decoration: underline;
-    }
-
-    .footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    background-color: white;
-    color: black;
-    text-align: center;
-    }
-    </style>
-    <div class="footer">
-    <p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.instagram.com/riccardoperotti_/" target="_blank">Riccardo Perotti</a></p>
+    footer="""
+    <div style='position: fixed;left: 0;bottom: 0;width: 100%; text-align: center;'>
+        <p>
+            Developed with ❤ by 
+                    <a href="https://www.instagram.com/riccardoperotti_/" target="_blank">
+                        Riccardo Perotti
+                    </a>
+                    Copyright © 2024 
+        </p>
     </div>
     """
-
-
     st.markdown(footer,unsafe_allow_html=True)
+
 
 
 
